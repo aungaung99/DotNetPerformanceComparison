b93b72d36d0bf5cfedba19a7bee92cac47aca369
Aung Naing Oo <aungaung20.ab@gmail.com>
2025-06-22 20:27:02 +0630

    added String Concat Benchmarks

---
+++ Benchmarks/StringAllocationBenchmarks.cs
@@ -0,0 +1,21 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace DotNetPerformanceComparison.Benchmarks;
+
+[MemoryDiagnoser]
+public class StringAllocationBenchmarks
+{
+    private const string SampleText = "BenchmarkDotNet is powerful!";
+
+    [Benchmark]
+    public string StringConcatOption1()
+    {
+        return "Benchmark" + "DotNet" + SampleText;
+    }
+
+    [Benchmark]
+    public string StringConcatOption2()
+    {
+        return $"Benchmark DotNet {SampleText}";
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using BenchmarkDotNet.Running;
+using DotNetPerformanceComparison.Benchmarks;
+
 Console.WriteLine("Hello, World!");
+
+BenchmarkRunner.Run<StringAllocationBenchmarks>();
